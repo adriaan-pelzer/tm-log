7b570bdfe2067092f2075c72f6709fc09490ba20
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2014-04-28 10:54:15 +0100

    Start logging module

---
+++ log.js
@@ -0,0 +1,19 @@
+var _ = require ( 'lodash' );
+var path = require ( 'path' );
+var log4js = require ( 'log4js' );
+
+exports.inspect = require ( 'eyes' ).inspector ( { maxLength: 0 } );
+
+exports.logger = function ( filename, config ) {
+    var fileNameRelativeToThisDir, module, logger;
+
+    fileNameRelativeToThisDir = path.relative ( __dirname, filename.replace ( path.extname ( filename ), '' ) );
+    module = fileNameRelativeToThisDir.split ( path.sep );
+
+    logger = log4js.getLogger ( module.join ( '.' ) );
+    logger.setLevel ( _.reduce ( module, function ( logLevel, mPart ) {
+        return _.isString ( logLevel ) ? logLevel : ( _.isUndefined ( logLevel[mPart] ) ? 'ERROR' : logLevel[mPart] );
+    }, config.logLevels ) );
+
+    return logger;
+};
